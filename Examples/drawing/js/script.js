f075f99dd4d99d970f294a1775d8f736f8e8a3b9
Jolene <joleneodia@hotmail.com>
2024-09-04 14:15:30 -0400

    Create the drawing module

---
+++ Examples/drawing/js/script.js
@@ -0,0 +1,27 @@
+/**
+ * Function Calls
+ * Jolene Bodika
+ * 
+ * Function calls examples for CART 253
+ * these are comments lol :))
+ */
+
+"use strict";
+
+// creates the canvas 
+
+function setup() {
+    // standard res canvas
+    createCanvas(640,480);
+}
+
+
+
+function draw() {
+    // pink desktop
+    background(150,230,176);
+    
+    // blank piece of paper
+    rect(200,80,240,320);
+
+}
