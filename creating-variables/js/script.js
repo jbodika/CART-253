23a065a4732f91d0e9503fe4ac8d11d622fb13e1
Jolene <joleneodia@hotmail.com>
2024-09-10 14:57:26 -0400

    Added variables to project

--- creating-variables/js/script.js
+++ creating-variables/js/script.js
@@ -1,24 +1,33 @@
 /**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ *Jolene Bodika
+* Examples on Creating Variables
  */
 
 "use strict";
+//colours for the cheese
+let cheeseRed = 255;
+let cheeseGreen = 255;
+let cheeseBlue = 255;
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+// coordinates for the cheese
+let holeShade = 0;
+let holeX = 140;
+let holeY = 175;
+let holeSize = 180;
+
 function setup() {
-
+    // Create the canvas
+    createCanvas(480, 480);
 }
 
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
-
+    // Cheese colour (yellow)
+    background(cheeseRed,cheeseGreen,cheeseBlue);
+    
+    // Draw a hole in the upper left
+    push();
+    noStroke();
+    fill(holeShade);
+    ellipse(holeX, holeY, holeSize);
+    pop();
 }
