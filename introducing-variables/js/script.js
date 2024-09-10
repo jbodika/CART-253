414da356cefad5c15128cfae9e856e8aad9b3a1b
Jolene <joleneodia@hotmail.com>
2024-09-10 14:47:36 -0400

    Added a circle that changes sizes and colours when the mouse moves

--- introducing-variables/js/script.js
+++ introducing-variables/js/script.js
@@ -16,7 +16,7 @@ function draw() {
     // Draw a circle in the centre of the canvas
     push();
     noStroke();
-    fill(255, 0, 0);
+    fill(mouseX, mouseY, 0);
     //changed the width and height to always keep the circle in the center and changed the numbers to use mouseX and mouseY variables in order to change the size of the circle based on where the mouse is placed
     ellipse(width/2, height/2, mouseX, mouseY);
     pop();
