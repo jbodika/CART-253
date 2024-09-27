844102b8e5aae96c103a521c3c0340df92d73b3a
Jolene <joleneodia@hotmail.com>
2024-09-26 22:14:30 -0400

    Created the base for the claw machine
    Changed the initial program to be a claw machine game and created the base for the machine

--- Assignments/ArtJam/js/index.js
+++ Assignments/ArtJam/js/index.js
@@ -1,66 +1,123 @@
+/*
+* Name: Jolene Bodika
+* Project: Art Jam Assignment 
+* Description: This program is a claw machine game.
+* Course: CART 253 - Creative Computing I 
+*/
+
+// CONSTANT VARIABLES
+
+const claw = {}
+
+const joystick = {}
+
+
+const colours = {
+  mainBlue:"#6b82e0",
+  secondaryBlue:"#1f3694",
+  shadingBlue:"#415fd7",
+  lightGray:"#D3D3D3",
+  darkGray:"#808080",
+  lightGreen:"#90EE90",
+  groundColour:"#c0caf2",
+  black:"#000",
+  white:"#fff",
+  backgroundColour: "#eaedfb"
+
+}
+
+
+// FUNCTIONS
+
 function setup() {
-    createCanvas(800, 800);
-  }
-  
-  //displays the canvas
-  function draw() {
-    background("#268b07");
-
-    drawMushroom();
-   
-  }
-  
-function drawMushroom(){
-//mushroom base
-push();
-fill("#ffeddf");
-ellipse(400,750,200,700);
-pop();
-
-//mushroom shadow
-push();
-noStroke();
-fill("#818589");
-ellipse(400,525,165,100);
-pop();
+  createCanvas(800, 800);
+  //angleMode(DEGREES);
+}
 
 
-//mushroom top
-push();
-fill("#9a1616");
-ellipse(400,400,400,300);
-pop();
 
-//mushroom dots
-push();
-fill("#fff8f3");
-ellipse(500,400,65,25);
-ellipse(450,320,55,40);
-ellipse(300,300,58,30);
-ellipse(300,475,55,35);
-ellipse(210,400,30,42);
-ellipse(345,400,35,23);
-ellipse(580,375,35,43);
-ellipse(510,490,60,41);
-ellipse(420,520,30,20);
-ellipse(460,450,30,20);
-pop();
+// displays the canvas
+function draw() {
+  background(colours.backgroundColour);
+
+  drawMachine();
 
 }
 
 
-function changeBackground(){
-    background("blue");
-}
-
-function drawGrasshopper(){}
-
-function drawBird(){}
-
-function drawSnake(){}
-
-function drawOwl(){}
+function drawMachine() {
 
 
+  //ground
+  push()
+  noStroke()
+  fill(colours.groundColour)
+  rect(0, 700, 800, 100)
+  pop()
+
+  //machine base
+  push();
+  noStroke()
+  fill(colours.mainBlue)
+  rect(200, 50, 400, 700)
+  rect(175, 720, 450, 40)
+  rect(175, 500, 450, 50)
+  rect(175, 40, 450, 70)
+
+  triangle(175, 500, 150, 580, 215, 580)
+  triangle(625, 500, 580, 580, 650, 580)
+  
+  fill(colours.shadingBlue)
+  rect(200, 580, 400, 10)
+  pop();
 
 
+  //white screen
+  push();
+  noStroke();
+  fill(colours.white);
+  rect(250, 80, 300, 420);
+  pop();
+
+  // air vents
+  push();
+  noStroke();
+  fill(colours.secondaryBlue);
+  rect(280, 520, 50, 10);
+  rect(280, 535, 50, 10);
+  rect(280, 550, 50, 10);
+
+  //coin slot
+  fill(colours.lightGray);
+  rect(250, 515, 20, 40);
+  fill(colours.black);
+  rect(257, 525, 5, 20);
+
+  //joystick bottom
+  fill(colours.secondaryBlue)
+  rect(400, 520, 70, 40)
+  fill(colours.black)
+  rect(257, 525, 5, 20)
+
+  //Joystick base
+  fill(colours.darkGray)
+  ellipse(435, 540, 30, 30);
+  fill(colours.secondaryBlue)
+
+  //Joystick Ball
+  ellipse(435, 540, 20, 20);
+  fill(colours.black)
+  ellipse(435, 510, 10, 70);
+  fill(colours.secondaryBlue)
+  ellipse(435, 480, 45, 45);
+
+  //Green machine button
+  fill(colours.secondaryBlue)
+  rect(500, 520, 40, 40)
+  fill(colours.lightGray)
+  ellipse(518, 540, 20, 20)
+  fill(colours.lightGreen)
+  ellipse(520, 540, 20, 20)
+  pop();
+
+} 
