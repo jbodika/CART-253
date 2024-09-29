5d7e22d11df44586e27bf5272d9cda2ed8c8e294
Jolene <joleneodia@hotmail.com>
2024-09-29 17:43:26 -0400

    Added user input functionality
    The claw and handle now interact with each other to move around

--- Assignments/ArtJam/js/index.js
+++ Assignments/ArtJam/js/index.js
@@ -1,12 +1,16 @@
 /*
-* Name: Jolene Bodika
-* Project: Art Jam Assignment 
-* Description: This program is a claw machine game.
-* Course: CART 253 - Creative Computing I 
+* Arcade Claw Machine Simulation
+* Jolene Bodika
+*
+* Controls:
+* - press and hold on the machine handle to move around
+*
+* Uses:
+* p5.js library
+* https://p5js.org
 */
 
 // CONSTANT VARIABLES
-
 const claw = {
   left: {
     x: 380,
@@ -20,13 +24,14 @@ const claw = {
 
 
 const handle = {
-  x: 435,
-  y: 480,
-  size: 45,
-
+  top: {
+    x: 435,
+    y: 480,
+    size: 45,
+  },
   bottom: {
-    x: Number,
-    y: Number
+    x: 435,
+    y: 510
   }
 }
 
@@ -45,17 +50,18 @@ const colours = {
   backgroundColour: "#eaedfb"
 
 }
-const clawChain = { x: 395, y: 110 };
+
+const clawChain = {
+  x: 395,
+  y: 110
+};
+
 // GLOBAL VARIABLES
-
-
 let mouseIsOverlapping;
 
 // FUNCTIONS
-
 function setup() {
   createCanvas(800, 800);
-  //angleMode(DEGREES);
 }
 
 
@@ -66,40 +72,46 @@ function draw() {
 
   drawMachine();
   moveHandle();
+  moveClaw();
   //drawPlushies()
 }
 
 
+
+
 function drawMachine() {
-
-
   //Ground area
-  push()
-  noStroke()
-  fill(colours.groundColour)
-  rect(0, 700, 800, 100)
-  pop()
+  noStroke();
+  push();
+  fill(colours.groundColour);
+  rect(0, 700, 800, 100);
+  pop();
 
   //Claw machine base
   push();
-  noStroke()
-  fill(colours.mainBlue)
-  rect(200, 50, 400, 700)
-  rect(175, 720, 450, 40)
-  rect(175, 500, 450, 50)
+  noStroke();
+  fill(colours.mainBlue);
+  rect(200, 50, 400, 700);
+  rect(175, 720, 450, 40);
+  rect(175, 500, 450, 50);
 
-  rect(175, 20, 450, 70)
+  rect(175, 20, 450, 70);
 
-  triangle(175, 500, 150, 580, 215, 580)
-  triangle(625, 500, 580, 580, 650, 580)
+  triangle(175, 500, 150, 580, 215, 580);
+  triangle(625, 500, 580, 580, 650, 580);
 
-  fill(colours.shadingBlue)
-  rect(200, 580, 400, 10)
+  fill(colours.shadingBlue);
+  rect(200, 580, 400, 10);
   pop();
 
 
   //toy slot
-  //TODO: CODE IT 
+  push();
+  noStroke();
+  rect(460, 600, 80, 70);
+  fill(colours.lightGray);
+  pop();
+
 
 
   //white screen
@@ -110,6 +122,9 @@ function drawMachine() {
   pop();
 
   drawPlushies();
+
+ 
+
   // air vents
   push();
   noStroke();
@@ -124,99 +139,154 @@ function drawMachine() {
   fill(colours.black);
   rect(257, 525, 5, 20);
 
-  //joystick bottom
-  fill(colours.secondaryBlue)
-  rect(400, 520, 70, 40)
-  fill(colours.black)
-  rect(257, 525, 5, 20)
+  //handle bottom
+  fill(colours.secondaryBlue);
+  rect(400, 520, 70, 40);
+  fill(colours.black);
+  rect(257, 525, 5, 20);
 
-  //Joystick base
-  fill(colours.darkGray)
+  //handle base
+  fill(colours.darkGray);
   ellipse(435, 540, 30, 30);
   fill(colours.secondaryBlue)
 
-  //Joystick Ball
-  ellipse(435, 540, 20, 20);
-  fill(colours.black)
-  ellipse(435, 510, 10, 70);
-  fill(colours.secondaryBlue)
-  ellipse(handle.x, handle.y, handle.size);
 
   //Green machine button
-  fill(colours.secondaryBlue)
-  rect(500, 520, 40, 40)
-  fill(colours.lightGray)
-  ellipse(518, 540, 20, 20)
-  fill(colours.lightGreen)
-  ellipse(520, 540, 20, 20)
+  fill(colours.secondaryBlue);
+  rect(500, 520, 40, 40);
+  fill(colours.lightGray);
+  ellipse(518, 540, 20, 20);
+  fill(colours.lightGreen);
+  ellipse(520, 540, 20, 20);
   pop();
 
   //claw
-  push()
-  noStroke()
-  fill(colours.lightGray)
-  rect(250, 80, 300, 20)
-  ellipse(clawChain.x + 5, 100, 15, 15)
+  push();
+  noStroke();
+  fill(colours.black);
+  rect(250, 80, 300, 20);
+  ellipse(clawChain.x + 5, 100, 15, 15);
   // clawchain
-  for (let x = clawChain.y; x < 300; x += 15) {
-    rect(clawChain.x, x, 10, 10)
-  }
+ rect(clawChain.x, clawChain.y, 10, 180);
+  // for (let x = clawChain.y; x < 450; x += 15) {
+  //   rect(clawChain.x, x, 10, 10);
+  // }
   rect(claw.left.x, claw.left.y, 10, 30);
   rect(claw.right.x, claw.right.y, 10, 30);
   pop();
 
+   // //reflective glass
+   push()
+
+   fill(167, 199, 203, 127);
+   rect(250, 80, 300, 410);
+ 
+   fill(219, 225, 227, 127);
+   noStroke();
+   triangle(250, 100, 550, 490, 550, 100);
+ 
+   pop();
+
+     //handle ball
+
+  push()
+  ellipse(435, 540, 20, 20);
+  fill(colours.black);      
+  ellipse(handle.bottom.x, handle.bottom.y, 10, 70);
+  fill(colours.secondaryBlue);
+  ellipse(handle.top.x, handle.top.y, handle.top.size);
+  pop()
+ 
 
 }
 
 
-function mousePressed() {
-  console.log('pressed')
-}
+
+
+
 
 
 function moveHandle() {
-  const distance = dist(mouseX, mouseY, handle.x, handle.y)
-  mouseIsOverlapping = (distance < handle.size / 2)
-  console.log(mouseIsOverlapping)
+ 
+  const distance = dist(mouseX, mouseY, handle.top.x, handle.top.y); // code snippet taken from the conditionals challenge
+  mouseIsOverlapping = (distance < handle.top.size / 2);
 
-  if (mouseIsOverlapping) {
+
+
+  if (mouseIsOverlapping && mouseIsPressed) {
+
+    handle.top.x = mouseX;
+    handle.bottom.x = mouseX;
+    handle.top.y = mouseY;
+
+    handle.top.x = constrain(handle.top.x, 425, 450);
+    handle.top.y = constrain(handle.top.y, 465, 490);
+    handle.bottom.x = constrain(handle.bottom.x, 430, 443);
+
+
+
+    clawChain.x = constrain(clawChain.x, 265, 525);
+    clawChain.y = constrain(clawChain.y, 110, 270);
+    claw.left.y = constrain(claw.left.y, 290, 450);
+    claw.right.y = constrain(claw.right.y, 290, 450);
+
+
+    claw.left.x = constrain(claw.left.x, 250, 510);
+    claw.right.x = constrain(claw.right.x, 280, 540);
+
+
+    axisX = handle.top.x - 395;
+    axisY = handle.top.y - 110
+
+    //ternary conditional operator they work exactly the same as an if function but they're just displayed in one line
+    // axis > 40 if yes increment by 1 if not decrement by 1
+    clawChain.x += (axisX > 40 ? 1 : -1);
+    clawChain.y +=(axisY > 370 ? -1 : 1);
+    claw.left.y +=(axisY > 370 ? -1 : 1);
+    claw.right.y +=(axisY > 370 ? -1 : 1);
+
+    claw.left.x += (axisX > 40 ? 1 : -1);
+    claw.right.x += (axisX > 40 ? 1 : -1);
+
+
   }
-  // handle.x= mouseX
-  // handle.y= mouseY
+
+
+
 }
 
 function moveClaw() {
-  //if moving right 
-  claw.left.x += 1;
-  claw.right.x += 1
-  //if moving left
-  //claw.leftX-=1;
-  //claw.rightX-=1
 
-  clawChain.x += 1
-  clawChain.x = constrain(clawChain.x, 265, 525)
-  claw.left.x = constrain(claw.left.x, 250, 510)
+  // let axisX = Number;
+  //   clawChain.x = constrain(clawChain.x, 265, 525);
+  //   claw.left.x = constrain(claw.left.x, 250, 510);
+  //   claw.right.x = constrain(claw.right.x, 280, 540);
 
-  claw.right.x = constrain(claw.right.x, 280, 540)
-}
+
+  //   axisX = handle.top.x - 395;
+
+  //   clawChain.x += (axisX > 40 ? 1 : -1);
+  //   claw.left.x += (axisX > 40 ? 1 : -1);
+  //   claw.right.x += (axisX > 40 ? 1 : -1);
+  }
 
 
 function drawPlushies() {
-  let colourArray = ['#baffab', '#9dfbd3', '#aaa8f9', '#deb0ff', '#eeb8cb']
-  push()
-  noStroke()
-  let linejump = 0
+  let colourArray = ['#f920aa', '#20aaf9', '#aaf920', '#ad03de', '#FF7518']
+  push();
+  noStroke();
+  let linejump = 0;
   for (let i = 265; i < 550; i += 30) {
     for (let j = 350; j < 500; j += 30) {
-      fill(colourArray.slice(-1))
+      fill(colourArray.slice(-1));
       ellipse(i, j, 30);
       // added functionality to change the colours of the toys
-      //every toy the linejump varibale gets incremented 
+      //every toy the linejump varibale gets incremented
       // once it reaches 10 in removes the last colour from the colourArray
-      linejump++
+      linejump++;
       if (linejump == 10) {
-        linejump = 0
-        colourArray.pop()
+        linejump = 0;
+        colourArray.pop();
 
       }
 
